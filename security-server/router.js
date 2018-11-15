bc9e0f39002c68bec870de46dc31b8f08e301134
Natalie Rubin <natrubin@udel.edu>
2018-11-15 10:38:23 -0500

    merge

--- security-server/router.js
+++ security-server/router.js
@@ -16,19 +16,11 @@ module.exports = function(app) {
     authRoutes.post('/login', AuthenticationController.login);
     // /api/auth/authorize -- GET -- needs authentication
     authRoutes.get('/authorize',passportService.requireAuth,AuthenticationController.authorize);
-<<<<<<< HEAD
 
     // /api/home/info -- GET -- needs authentication
     otherRoutes.get('/info',passportService.requireAuth,function(req,res,next){
         res.json({user: req.user.toJson()})});
     otherRoutes.get('/mylists');
     apiRoutes.use('/home',otherRoutes);
-=======
-    // /api/stuff/info -- GET -- needs authentication
-    otherRoutes.get('/info',passportService.requireAuth,function(req,res,next){
-        res.json({user: req.user.toJson()})});
-        
-    apiRoutes.use('/stuff',otherRoutes);
->>>>>>> 76e1d02f05e883734fe0018f9c4583fece3bfac1
     app.use('/api', apiRoutes);
 };
