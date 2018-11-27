983529eff4eac1109e24f4d86b396b4596daaf31
Natalie Rubin <natrubin@udel.edu>
2018-11-27 10:22:02 -0500

    added to task controller

--- security-server/controllers/tasks.js
+++ security-server/controllers/tasks.js
@@ -2,9 +2,19 @@ exports.editTask = function (req, res, next) {
     const name = req.body.name;
     const dueDate = req.body.dueDate;
     const details = req.body.details;
+    let task = req.body.task;
 
     //not sure what to actually return
+    let taskInfo = task.toJson();    
     return res.status(200).json({
-        validated: true
+        validated: true,
+        task: taskInfo
+    })
+}
+exports.deleteTask = function (req, res, next) {
+    const task = req.body.task;
+
+    return res.status(200).json({
+        task: null
     })
 }
