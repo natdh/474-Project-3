2f82258e8ca2e76e10b457ab4609452d44763635
Evil-Fishy <43159174+Evil-Fishy@users.noreply.github.com>
2018-12-04 10:37:52 -0500

    fixed red stuff

--- my-app/src/app/views/home/home.component.ts
+++ my-app/src/app/views/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SecurityService } from '../../security/services/security.service';
-import { ListService } from '../../list.service';
+import { ListService } from '../../lists/list.service';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
 loggedIn = false;
   private name: string;
   private desc: string;
-  lists: List[];
+  lists: ListService[];
   constructor(private _secSvc: SecurityService) { this.name = this.desc = ''; }
 
   ngOnInit() {
