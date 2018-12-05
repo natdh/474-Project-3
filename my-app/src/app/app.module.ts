df93556f8bdda8032d01892069548e6d927931f6
Ashley Gold <angold@udel.edu>
2018-12-04 22:53:52 -0500

    useless my lists tab removed

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { SecurityModule } from './security/security.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
-import { SecureComponent } from './views/my-lists/secure.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SecureComponent,
   ],
   imports: [
     BrowserModule,
