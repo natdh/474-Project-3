94d64316c8df4d61c13c48c76eedf36338d5b43e
Evil-Fishy <43159174+Evil-Fishy@users.noreply.github.com>
2018-12-05 15:12:01 -0500

    can get log lists now

--- my-app/src/app/views/home/home.component.ts
+++ my-app/src/app/views/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SecurityService } from '../../security/services/security.service';
 import { UserService } from '../../security/services/user.service';
+import {LocalStorageService} from 'angular-2-local-storage';
+
 
 @Component({
   selector: 'app-home',
@@ -18,10 +20,14 @@ export class HomeComponent implements OnInit {
   private listid: string;
   private desc: string;
   private tasks: Array<string>;
-  constructor(private _secSvc: SecurityService, private _userSvc: UserService) { this.name = this.desc = ''; }
+  private lists: [];
+  constructor(private _secSvc: SecurityService, private _userSvc: UserService, private _localStorage: LocalStorageService) { this.name = this.desc = ''; }
 
   getUserLists = () => {
-    lists: [] = this._userSvc.getUser()[1].lists;
+    //this.lists = this._localStorage.get('lists');
+    //console.log(this.lists);
+    //this._userSvc.getUser().json()['user'].lists.forEach(function(item){item.forEach(function(inner){console.log(inner);})});
+    console.log(this._userSvc.getUser()['user']['lists']);
   }
 
   logLists() {
